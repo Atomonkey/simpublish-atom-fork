ea39c180cb3f98cff275171ff80782fb3721c94d
Atomonkey <6678755+Atomonkey@users.noreply.github.com>
2025-02-09 02:14:12 +0800

    A significant refinement

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,15 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
+		files: {
+			assets: 'static',
+			routes: 'src/routes'
+		},
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		//Override http methods in the Todo forms
+		// methodOverride: {
+		// 	allowed: ['PATCH', 'DELETE']
+		// }
 	}
 };
 
